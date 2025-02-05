a9cca22aec5c27d0c4e885c0edfe678eee15b4cb
z3nith328 <150067003+z3nith328@users.noreply.github.com>
2025-02-05 16:56:27 -0600

    Update updatereorderpoint.sql

--- updatereorderpoint.sql
+++ updatereorderpoint.sql
@@ -1,3 +1,14 @@
+CREATE OR REPLACE FUNCTION update_reorder_point()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE inventory_safety_stock
+    SET reorder_point = safety_stock_level + (SELECT AVG(lead_time_variability * demand_variability) FROM inventory_safety_stock)
+    WHERE item_id = NEW.item_id;
+   
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TRIGGER trg_update_reorder_point
 AFTER UPDATE ON inventory_safety_stock
 FOR EACH ROW
