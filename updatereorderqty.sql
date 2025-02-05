883574963d255cbf0f178760815bde5871c5700f
z3nith328 <150067003+z3nith328@users.noreply.github.com>
2025-02-05 17:17:19 -0600

    Create updatereorderqty.sql

---
+++ updatereorderqty.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION update_reorder_qty_on_stock_change()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE resupply_schedule
+    SET reorder_qty = iss.reorder_point
+    FROM inventory_safety_stock iss
+    WHERE iss.item_id = NEW.item_id AND resupply_schedule.item_id = NEW.item_id;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER TR_Update_Resupply_On_Stock_Change
+AFTER UPDATE ON inventory_levels
+FOR EACH ROW
+EXECUTE FUNCTION update_reorder_qty_on_stock_change();
